0cc9881ee7467f738ab508c3137157ce5c9d7df0
eparizoto <eparizoto@gmail.com>
2024-10-08 08:16:01 -0300

    feat: listar e buscar por ID

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
+class TopicoService(private var topicos: List<Topico>) {
 
-    fun listar(): List<Topico> {
-        val topico = Topico(id = 1,
-            titulo = "Duvida Kotlin",
-            mensagen = "Variáveis no Kotlin",
+    init {
+        val topico1 = Topico(
+            id = 1,
+            titulo = "Duvida Kotlin 1",
+            mensagen = "Variáveis no Kotlin 1",
             curso = Curso(id = 1,
                 nome = "Kotlin",
                 categoria = "Programacao"),
@@ -21,6 +22,41 @@ class TopicoService {
                 email = "ana@email.com")
         )
 
-        return Arrays.asList(topico, topico, topico);
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida Kotlin 2",
+            mensagen = "Variáveis no Kotlin 2",
+            curso = Curso(id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"),
+            autor = Usuario(id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com")
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvida Kotlin 3",
+            mensagen = "Variáveis no Kotlin 3",
+            curso = Curso(id = 1,
+                nome = "Kotlin",
+                categoria = "Programacao"),
+            autor = Usuario(id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com")
+        )
+
+        topicos =  Arrays.asList(topico1, topico2, topico3)
+    }
+
+    fun listar(): List<Topico> {
+        return topicos;
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream()
+            .filter({t -> t.id == id})
+            .findFirst()
+            .get()
     }
 }
